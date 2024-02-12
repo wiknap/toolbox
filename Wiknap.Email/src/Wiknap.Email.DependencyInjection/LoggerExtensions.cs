040dfc82ab6d6e3e5a3db672510bbf1715cb234b
Wiktor Napierała <wiktor.napierala@outlook.com>
2024-02-12 22:54:41 +0100

    Logging level change

--- Wiknap.Email/src/Wiknap.Email.DependencyInjection/LoggerExtensions.cs
+++ Wiknap.Email/src/Wiknap.Email.DependencyInjection/LoggerExtensions.cs
@@ -4,21 +4,21 @@ namespace Wiknap.Email.DependencyInjection;
 
 public static partial class LoggerExtensions
 {
-    [LoggerMessage(EventId = 1, Level = LogLevel.Trace, Message = "Sending email to {email}")]
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Sending email to {email}")]
     public static partial void SendingEmail(this ILogger logger, string email);
 
-    [LoggerMessage(EventId = 2, Level = LogLevel.Trace, Message = "Email sent to {email}")]
+    [LoggerMessage(EventId = 2, Level = LogLevel.Information, Message = "Email sent to {email}")]
     public static partial void EmailSent(this ILogger logger, string email);
 
-    [LoggerMessage(EventId = 3, Level = LogLevel.Trace, Message = "Searching for email")]
+    [LoggerMessage(EventId = 3, Level = LogLevel.Information, Message = "Searching for email")]
     public static partial void SearchingEmail(this ILogger logger);
 
-    [LoggerMessage(EventId = 4, Level = LogLevel.Trace, Message = "Email found")]
+    [LoggerMessage(EventId = 4, Level = LogLevel.Information, Message = "Email found")]
     public static partial void EmailFound(this ILogger logger);
 
-    [LoggerMessage(EventId = 5, Level = LogLevel.Trace, Message = "Email not found")]
+    [LoggerMessage(EventId = 5, Level = LogLevel.Information, Message = "Email not found")]
     public static partial void EmailNotFound(this ILogger logger);
 
-    [LoggerMessage(EventId = 6, Level = LogLevel.Trace, Message = "Email is excluded {email}")]
+    [LoggerMessage(EventId = 6, Level = LogLevel.Information, Message = "Email is excluded {email}")]
     public static partial void EmailExcluded(this ILogger logger, string email);
 }
