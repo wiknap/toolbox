b5aa1a45f46f0a970f3021cbb965f5e0a29627fd
Wiktor Napierała <wiktor.napierala@outlook.com>
2024-07-24 22:10:21 +0200

    Add unit tests for PostRefundResponse serialization
    Introduce tests to ensure correct serialization and deserialization of the PostRefundResponse model. These tests check that JSON output and object recovery are consistent with expected values.

---
+++ Wiknap.PayNow/test/Wiknap.PayNow.Tests.Unit/Model/PostRefundResponseTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+
+using Bogus;
+
+using Shouldly;
+
+using Wiknap.PayNow.Model;
+
+using Xunit;
+
+namespace Wiknap.PayNow.Tests.Unit.Model;
+
+public sealed class PostRefundResponseTests : TestsBase
+{
+    [Fact]
+    public void Given_ValidRequest_When_Serialize_Then_ReturnsCorrectJson()
+    {
+        // Arrange
+        var request = new PostRefundResponse
+        {
+            RefundId = Faker.Lorem.Word(),
+            Status = PostRefundStatus.New
+        };
+
+        // Act & Assert
+        var deserialized = JsonSerializer.Serialize(request);
+
+        deserialized.ShouldBe(
+            $"{{" +
+                $"\"refundId\":\"{request.RefundId}\"," +
+                $"\"status\":\"NEW\"" +
+            $"}}");
+    }
+
+    [Fact]
+    public void Given_ValidJson_When_Deserialize_Then_ReturnsObject()
+    {
+        // Arrange
+        var id = Faker.Lorem.Word();
+        var json =
+            $"{{" +
+                $"\"refundId\":\"{id}\"," +
+                $"\"status\":\"NEW\"" +
+            $"}}";
+
+        // Act & Assert
+        var deserialized = JsonSerializer.Deserialize<PostRefundResponse>(json);
+
+        deserialized.ShouldNotBeNull();
+        deserialized.RefundId.ShouldBe(id);
+        deserialized.Status.ShouldBe(PostRefundStatus.New);
+    }
+}
